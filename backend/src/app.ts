bda331d6a93337c839741064fe1fdaf1eee1a6fb
pmtking <mohamadtaher6674@gmail.com>
2025-08-17 18:29:14 +0330

    tets

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,6 +15,9 @@ app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "/public/uploads/")));
 dotenvConfig();
 app.use("/api", router);
+app.get('/t' , (req , res) => {
+  return res.send("welcom to backend")
+})
 
 export const startServer = async () => {
   try {
